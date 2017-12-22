cbda48d2894b148610d5734fc8367f61be857793
Miguel Muscat <miguelmuscat93@gmail.com>
2017-12-22 11:13:03 +0100

    Added `PdoInsertSqlCapableTrait` and test

---
+++ src/Pdo/PdoInsertCapableTrait.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace RebelCode\Storage\Resource\Pdo;
+
+use Dhii\Util\String\StringableInterface as Stringable;
+use PDOStatement;
+
+/**
+ * Common functionality for objects that can insert records into a database using PDO.
+ *
+ * @since [*next-version*]
+ */
+trait PdoInsertCapableTrait
+{
+    /**
+     * Executes an INSERT SQL query, inserting records into the database.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $records A list of containers, each containing the record data to insert.
+     *
+     * @return PDOStatement The executed PDO statement.
+     */
+    protected function _insert(array $records)
+    {
+        $valueHashMap = array_map([$this, '_getSqlInsertValueHashMap'], $records);
+
+        $query = $this->_buildInsertSql(
+            $this->_getSqlInsertTable(),
+            $this->_getSqlInsertColumnNames(),
+            $records,
+            $valueHashMap
+        );
+
+        $statement = $this->_executePdoQuery($query);
+        $statement->execute($valueHashMap);
+
+        return $statement;
+    }
+
+    /**
+     * Extracts and retrieves the data hash map from record data.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $record An associative array mapping field names to their values for a single record.
+     *
+     * @return array The generated value hash map, mapping values to their respective hashes.
+     */
+    protected function _getSqlInsertValueHashMap($record)
+    {
+        $map = [];
+
+        foreach ($this->_getSqlInsertFieldColumnMap() as $_field => $_column) {
+            if (!isset($record[$_field])) {
+                continue;
+            }
+
+            $_value = $record[$_field];
+            $_hash  = $this->_getPdoValueHashString($_value);
+
+            $map[$_column] = $_hash;
+        }
+
+        return $map;
+    }
+
+    /**
+     * Hashes a query value for use in PDO queries when parameter binding.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $value The value to hash.
+     *
+     * @return string The string hash.
+     */
+    abstract protected function _getPdoValueHashString($value);
+
+    /**
+     * Builds an INSERT SQL query.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable     $table        The name of the table to insert into.
+     * @param string[]|Stringable[] $columns      A list of columns names. The order is preserved in the built query.
+     * @param array                 $rowSet       The record data as a map of column names to values.
+     * @param array                 $valueHashMap Optional map of value names and their hashes.
+     *
+     * @return string The built INSERT query.
+     */
+    abstract protected function _buildInsertSql(
+        $table,
+        array $columns,
+        array $rowSet,
+        array $valueHashMap = []
+    );
+
+    /**
+     * Retrieves the SQL database table name for use in SQL INSERT queries.
+     *
+     * @since [*next-version*]
+     *
+     * @return string|Stringable The table.
+     */
+    abstract protected function _getSqlInsertTable();
+
+    /**
+     * Retrieves the names of the columns for use in SQL INSERT queries.
+     *
+     * @since [*next-version*]
+     *
+     * @return string[]|Stringable[] A list of column names.
+     */
+    abstract protected function _getSqlInsertColumnNames();
+
+    /**
+     * Retrieves the fields-to-columns mapping.
+     *
+     * @since [*next-version*]
+     *
+     * @return array A map containing the field names as keys and the matching column names as values.
+     */
+    abstract protected function _getSqlInsertFieldColumnMap();
+
+    /**
+     * Executes a given SQL query using PDO.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable $query     The query to invoke.
+     * @param array             $inputArgs The input arguments to use when executing the query.
+     *
+     * @return PDOStatement The executed statement.
+     */
+    abstract protected function _executePdoQuery($query, array $inputArgs = []);
+}
---
+++ test/functional/Pdo/PdoInsertCapableTraitTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace RebelCode\Storage\Resource\Pdo\FuncTest;
+
+use PHPUnit_Framework_MockObject_MockObject;
+use Xpmock\TestCase;
+
+/**
+ * Tests {@see RebelCode\Storage\Resource\Pdo\PdoInsertCapableTrait}.
+ *
+ * @since [*next-version*]
+ */
+class PdoInsertCapableTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\Storage\Resource\Pdo\PdoInsertCapableTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $methods Optional additional mock methods.
+     *
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    public function createInstance(array $methods = [])
+    {
+        $builder = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+                        ->setMethods(
+                            array_merge(
+                                $methods,
+                                [
+                                    '_getPdoValueHashString',
+                                    '_buildInsertSql',
+                                    '_getSqlInsertTable',
+                                    '_getSqlInsertColumnNames',
+                                    '_getSqlInsertFieldColumnMap',
+                                    '_executePdoQuery',
+                                ]
+                            )
+                        );
+
+        $mock = $builder->getMockForTrait();
+        $mock->method('_normalizeString')->willReturn(
+            function ($input) {
+                return strval($input);
+            }
+        );
+        $mock->method('_getPdoValueHashString')->willReturn(
+            function ($input) {
+                return ':'.hash('crc32b', strval($input));
+            }
+        );
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'An instance of the test subject could not be created'
+        );
+    }
+
+    /**
+     * Tests the insert query method.
+     *
+     * @since [*next-version*]
+     */
+    public function testInsert()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $rowSet = [
+            [
+                'userId' => 5, 'userName' => 'foo', 'userAge' => 22,
+                'userId' => 11, 'userName' => 'bar', 'userAge' => 19,
+            ],
+        ];
+
+        $subject->method('_getSqlInsertTable')->willReturn($table = 'users');
+        $subject->method('_getSqlInsertColumnNames')->willReturn($cols = ['id', 'name', 'age']);
+        $subject->method('_getSqlInsertFieldColumnMap')->willReturn(
+            $map = [
+                'userId' => 'id',
+                'userName' => 'name',
+                'userAge' => 'age',
+            ]
+        );
+        $statement = $this->getMockBuilder('PDOStatement')
+            ->setMethods(['execute'])
+            ->getMockForAbstractClass();
+        $subject->method('_executePdoQuery')->willReturn($statement);
+
+        // The expectations ARE the test
+        $subject->expects($this->once())
+                ->method('_buildInsertSql')
+                ->with($table, $cols, $rowSet, $this->anything())
+                ->willReturn('INSERT INTO `users` (`id`, `name`, `age`) VALUES (5, "foo", 22), (11, "bar", 19)');
+
+        $statement->expects($this->once())
+            ->method('execute')
+            ->with($this->isType('array'));
+
+        $reflect->_insert($rowSet);
+    }
+}
